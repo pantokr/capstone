3249a007c9a23fbe5ccd9bb188b616a84fb8eaab
pantokrator <pantokrator3841@gmail.com>
2022-04-06 00:13:06 +0900

    0406_Song_onAuthState

--- public/mypage.js
+++ public/mypage.js
@@ -1,5 +1,5 @@
-import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import {getDatabase, ref, get, child, set} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getDatabase, ref, get, child, set } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 import './auth_google_signout.js'
 
 const profile_img = document.getElementById('profile_img');
@@ -12,30 +12,41 @@ const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("Logged in user.");
-        const db = getDatabase();
-        const dbRef = ref(db);
-        get(child(dbRef, `users/${user.uid}`))
-            .then((snapshot) => {
-                if (snapshot.exists()) {
-                    console.log(snapshot.val());
-                    var uid = user.uid;
-                    var val = snapshot.val();
+        // const db = getDatabase();
+        // const dbRef = ref(db);
+        // get(child(dbRef, `users/${user.uid}`))
+        //     .then((snapshot) => {
+        //         if (snapshot.exists()) {
+        //             console.log(snapshot.val());
+        //             var uid = user.uid;
+        //             var val = snapshot.val();
 
-                    var name = val.username;
-                    var email = val.email;
-                    var profile = val.profile_picture;
+        //             var name = val.username;
+        //             var email = val.email;
+        //             var profile = val.profile_picture;
 
-                    profile_img.setAttribute('src', profile);
-                    uid_span.innerText = uid;
-                    email_span.innerText = email;
-                    name_span.innerText = name;
-                } else {
-                    console.log("No data available");
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        //             profile_img.setAttribute('src', profile);
+        //             uid_span.innerText = uid;
+        //             email_span.innerText = email;
+        //             name_span.innerText = name;
+        //         } else {
+        //             console.log("No data available");
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         console.error(error);
+        //     });
+
+        var uid = user.uid;
+
+        const name = user.displayName;
+        const email = user.email;
+        const profile = user.photoURL;
+
+        profile_img.setAttribute('src', profile);
+        uid_span.innerText = uid;
+        email_span.innerText = email;
+        name_span.innerText = name;
     } else {
         console.log("No logged in user.");
         // User is signed out ...
