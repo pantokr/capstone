1cb0ae8e3fc4e203b03a7e6b00fee1de6c61382e
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2022-04-11 15:33:02 +0900

    0411_Jeong_dbUpdate_onMypage

--- public/mypage.js
+++ public/mypage.js
@@ -4,8 +4,18 @@ import './auth_google_signout.js'
 
 const profile_img = document.getElementById('profile_img');
 const uid_span = document.getElementById('uid_span');
-const name_span = document.getElementById('name_span');
 const email_span = document.getElementById('email_span');
+const name_span = document.getElementById('name_span');
+const age_span = document.getElementById('age_span');
+const gender_span = document.getElementById('gender_span');
+
+// const modal = document.getElementById('modal');
+const changed_name = document.getElementById('floatingName');
+const changed_year = document.getElementById('floatingYear');
+const changed_month = document.getElementById('floatingMonth');
+const changed_date = document.getElementById('floatingDate');
+const changed_gender = document.getElementById('floatingGender');
+const updateBtn = document.getElementById('updateBtn');
 
 const auth = getAuth();
 
@@ -16,35 +26,54 @@ onAuthStateChanged(auth, (user) => {
 
         const db = getDatabase();
         const dbRef = ref(db);
-
+        
+        console.log(db);
+        console.log(dbRef);
+        
         var val = null;
+
         get(child(dbRef, `users/${uid}`))
             .then((snapshot) => {
                 if (snapshot.exists()) {
                     val = snapshot.val();
 
-                    const name = val.username;
-                    const gender = val.gender;
-                    const email = val.email;
                     const profile = val.profile_picture;
+                    const name = val.username;
+                    const email = val.email;
                     const birth = val.birth;
                     const birth_y = val.birth.birth_year;
-                    const birth_m = val.birth.birth_year;
-                    const birth_d = val.birth.birth_year;
+                    const birth_m = val.birth.birth_month;
+                    const birth_d = val.birth.birth_date;
+                    const gender = val.gender;
 
-                    console.log(name);
-                    console.log(gender);
-                    console.log(email);
                     console.log(profile);
+                    console.log(email);
+                    console.log(name);
                     console.log(birth);
+                    console.log(gender);
+                    console.log(birth_y);
+                    console.log(birth_m);
+                    console.log(birth_d);
 
                     profile_img.setAttribute('src', profile);
+
                     uid_span.innerText = uid;
                     email_span.innerText = email;
                     name_span.innerText = name;
+                    age_span.innerText = `${birth_y} . ${birth_m} . ${birth_d}`;
+                    gender_span.innerText = gender === "male" ? "남자" : "여자";
+
+                    changed_name.value = name;
+                    changed_year.value = birth_y;
+                    changed_month.value = birth_m;
+                    changed_date.value = birth_d;
+                    changed_gender.value = gender;
+
                 } else {
                     console.log('Not in database');
                 }
+
+                
             })
             .catch((error) => {
                 console.error(error);
@@ -55,3 +84,15 @@ onAuthStateChanged(auth, (user) => {
         // User is signed out ...
     }
 });
+function handleUpdateonClick(){
+    
+    document.querySelector(".modal").classList.add("hidden");
+
+    
+    name_span.innerText = changed_name.value;
+    age_span.innerText = `${changed_year.value} . ${changed_month.value} . ${changed_date.value}`;
+    gender_span.innerText = changed_gender.value === "male" ? "남자" : "여자";
+    
+    
+}
+updateBtn.addEventListener("click", handleUpdateonClick);
