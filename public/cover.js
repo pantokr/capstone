a9ffe0cfc2dffb394650aadf0b6fa8009800efce
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2022-04-16 16:33:38 +0900

    0416_Jeong_scriptTojs

--- public/cover.js
+++ public/cover.js
@@ -22,3 +22,26 @@ function init() {
 
 init();
 
+const open = () => {
+  document
+      .querySelector(".modal")
+      .classList
+      .remove("hidden");
+}
+
+const close = () => {
+  document
+      .querySelector(".modal")
+      .classList
+      .add("hidden");
+}
+
+document
+  .querySelector(".openBtn")
+  .addEventListener("click", open);
+document
+  .querySelector(".close")
+  .addEventListener("click", close);
+document
+  .querySelector(".bg")
+  .addEventListener("click", close);
