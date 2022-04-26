91253f5c017e6032ef0476c48ff6aed84af5f07c
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-04-26 21:11:52 +0900

    0426_Joo_update_meeting_start

--- public/app.js
+++ public/app.js
@@ -209,6 +209,7 @@ async function joinRoomById(roomId) {
   }
 }
 
+// 내 카메라 띄우기
 async function openUserMedia(e) {
   const stream = await navigator.mediaDevices.getUserMedia(
     { video: true, audio: true });
@@ -349,3 +350,39 @@ var adjustHalfSize = function () {
   rq.style.height = "45vh";
 }
 
+
+
+
+
+//------------------------------STT----------------------------------------//
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'ko-KR';
+
+let makeNewTextContent = function () {
+    p = document.createElement('p');
+    document.querySelector('.chatLog').appendChild(p);
+};
+
+let p = null;
+
+recognition.start();
+recognition.onstart = function () {
+    makeNewTextContent(); // 음성 인식 시작시마다 새로운 문단을 추가한다.
+};
+recognition.onend = function () {
+    recognition.start();
+};
+
+recognition.onresult = function (e) {
+    let texts = Array.from(e.results)
+        .map(results => results[0].transcript).join("");
+
+    texts.replace(/느낌표|강조|뿅/gi, '❗️');
+
+    p.textContent = texts;
+};
+
+//-----------------------------------------------------------------------------------------------------------------
